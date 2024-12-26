c5d542ece9982fddf8c7619748d91e6bed0dcffa
AVDHANUSH <dhanush94900@gmail.com>
2024-12-26 12:50:46 +0530

    props concept

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,6 +1,15 @@
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Greeting from './Greeting'
 function App() {
   return (
+    <div>
     <h1>Hello from ReactJs</h1>
+    <Greeting name = {'Akash'}/>
+    <Greeting name = {'Ayush'}/>
+    <Greeting name = {'Ayushi'}/>
+    </div>
   );
 }
 export default App;
